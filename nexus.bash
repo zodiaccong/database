f6d2147e01dcf52dada409c09a36b78a014a7208
zodiaccong <52649950+zodiaccong@users.noreply.github.com>
2025-02-22 04:23:47 +0700

    Create nexus.bash

---
+++ nexus.bash
@@ -0,0 +1,12 @@
+sudo apt update -y
+sudo apt install unzip tmux git -y
+sudo apt install build-essential -y
+sudo apt install -y pkg-config libssl-dev -y
+cd /usr/local
+sudo wget https://github.com/protocolbuffers/protobuf/releases/download/v29.3/protoc-29.3-linux-x86_64.zip
+sudo unzip protoc-29.3-linux-x86_64.zip
+sudo chmod +x /usr/local/bin/protoc
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
+source $HOME/.cargo/env
+curl https://cli.nexus.xyz/ | sh
